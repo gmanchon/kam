ae607bc6667307499db98e713fbf77764881d6fb
Gaetan Manchon <gmanchon@gmail.com>
2021-07-24 00:37:14 +0200

    add missing created_at and updated_at instance variables to active record generated models

--- kam/templates/model.py
+++ kam/templates/model.py
@@ -9,8 +9,10 @@ class {{model_klass_name}}(ActiveRecord):
         # retrieve {{model_klass_name.lower()}} attributes
         self.id = kwargs.get("id"){% for instance_variable, _ in instance_variable_types.items() %}
         self.{{instance_variable}} = kwargs.get("{{instance_variable}}"){% endfor %}
+        self.created_at = kwargs.get("created_at")
+        self.updated_at = kwargs.get("updated_at")
 
     def __repr__(self):
 
-        return f"#<{{model_klass_name}} @={id(self)} id={self.id}{% for instance_variable, _ in instance_variable_types.items() %} {{instance_variable}}={self.{{instance_variable}}}{% endfor %}>"
+        return f"#<{{model_klass_name}} @={id(self)} id={self.id}{% for instance_variable, _ in instance_variable_types.items() %} {{instance_variable}}={self.{{instance_variable}}}{% endfor %} created_at={self.created_at} updated_at={self.updated_at}>"
 
