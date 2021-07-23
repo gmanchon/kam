b20647dbed3c356fee2af5461a3619721c0bbad9
Gaetan Manchon <gmanchon@gmail.com>
2021-07-23 10:01:23 +0200

    add streamlit radio helper

---
+++ kam/app/helpers/streamlit.py
@@ -0,0 +1,83 @@
+
+import streamlit as st
+
+import re
+
+
+MODEL_CLASS_ID_SEP = "___"
+
+loaded_models = {}
+
+
+def __build_model_string_id(model_klass, model_id):
+
+    return f"{model_klass}{MODEL_CLASS_ID_SEP}{model_id}"
+
+
+def __build_text_from_model(model_desc, model_id):
+
+    model_text = f"{model_desc} ({model_id})"
+
+    return model_text
+
+
+def __retrieve_id_from_model_text(model_text):
+
+    matches = re.compile(r"\([^\(]*\)$").search(model_text)
+
+    # remove parenthesis
+    model_id = matches[0][1:-1]
+
+    return model_id
+
+
+def model_to_text(model, model_desc):
+    """
+    returns reversible unique model textual representation
+    """
+
+    # build model string id
+    model_string_id = __build_model_string_id(model.__class__, model.id)
+
+    # store model
+    loaded_models[model_string_id] = model
+
+    # build textual representation
+    model_text = __build_text_from_model(model_desc, model.id)
+
+    return model_text
+
+
+def text_to_model(model_text, models):
+    """
+    retrieve model attributes from textual representation
+    """
+
+    # retrieve model id from textual representation
+    model_id = __retrieve_id_from_model_text(model_text)
+
+    # build model string id
+    model_string_id = __build_model_string_id(models[0].__class__, model_id)
+
+    # retrieve model
+    return loaded_models[model_string_id]
+
+
+def st_radio(label, models, model_formatter):
+    """
+    streamlit radio helper allowing to select a model from a list of models
+    """
+
+    # format model choices
+    model_choices = [model_to_text(m, model_formatter(m)) for m in models]
+
+    # create radio button
+    selected_assessment = st.radio(
+        label,
+        model_choices,
+        key=f"st_radio_{label}")
+
+    # convert selected model
+    model = text_to_model(selected_assessment, models)
+
+    return model
