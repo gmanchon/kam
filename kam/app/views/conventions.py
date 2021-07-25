dce6415da280d8e6331bd100c148294b92e1d220
Gaetan Manchon <gmanchon@gmail.com>
2021-07-25 18:32:54 +0200

    add helper to return unique aliases for a list of tables

--- kam/app/views/conventions.py
+++ kam/app/views/conventions.py
@@ -26,6 +26,15 @@ def singularize(model_name):
     return model_name[:-1]
 
 
+def is_plural(model_name):
+    """
+    whether model name is plural
+    """
+
+    # TODO: is plural
+    return model_name[-1:].lower() == "s"
+
+
 def get_app_directory_path():
     """
     return app directory
@@ -201,3 +210,41 @@ def model_name_to_klass_name(model_name):
     """
 
     return "".join([w.capitalize() for w in model_name.split("_")])
+
+
+def retrieve_table_alias(source_table_name, through):
+    """
+    return unique table aliases
+    """
+
+    # build table list
+    tables = [source_table_name] + through
+    aliases = []
+
+    # iterate through tables
+    for table in tables:
+
+        # search for alias, full table name should be enough at worst at the moment
+        alias_candidate = None
+        for index in range(len(table)):
+
+            # build alias candidate
+            alias_candidate = table[:index + 1]
+
+            # test alias availability
+            if alias_candidate not in aliases:
+
+                # alias is valid
+                break
+
+        # store alias
+        aliases.append(alias_candidate)
+
+    return aliases[0], aliases[1:]
+
+
+if __name__ == '__main__':
+
+    source_alias, through_alias = retrieve_table_alias(
+        "a_test_table", through=["abc", "abcdef", "b"])
+    print(source_alias, through_alias)
